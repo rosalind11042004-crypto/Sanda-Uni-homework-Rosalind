692566904c3987b5fbe2f6c122a1bcab9131756c
魏坤然 <2318996023@qq.com>
2026-03-03 16:21:04 +0800

    Finish tutorial02: add Choice model and register admin

--- polls/admin.py
+++ polls/admin.py
@@ -4,4 +4,8 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Question
 
+from django.contrib import admin
+from .models import Question, Choice
+
 admin.site.register(Question)
+admin.site.register(Choice)
